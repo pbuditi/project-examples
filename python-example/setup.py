b976188b5768f6afb08132e9c1477ac1f594a61d
pbuditi <pbuditi@gmail.com>
2016-10-15 16:29:54 +0530

    Update setup.py
    test

--- python-example/setup.py
+++ python-example/setup.py
@@ -8,7 +8,7 @@ setup(
     description="An example of how to create and publish to pypi.org",
     author="Michal Reuven",
     author_email="michal@jfrog.com",
-    url="http://localhost:8081/artifactory/api/pypi/pypi-local/simple",
+    url="http://localhost:8081/artifactory/project-example/project-python",
     version="0.1",
     install_requires=["nose",],
     packages=["pythonProj","tests",],
